e493a4cacc1b8e91ea62717067cf76a3cb8d9c90
Cameron Chrobocinski <cameronchrobo@gmail.com>
2026-07-10 13:58:11 -0500

    basic layout and mockup class display

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,44 @@
-import Image from "next/image";
+import BuildInfo from "./lib/build/BuildInfo";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    return (
+        // page container
+        <div className="flex flex-col flex-1 items-center justify-center font-sans">
+            {/* content container */}
+            <main className="flex flex-1 w-full h-full flex-col items-center justify-baseline p-4 sm:items-start">
+                {/* Header */}
+                <div className="flex flex-col w-full items-center justify-center align-center">
+                    <h1 className="text-3xl font-bold align-center">SCHOLAR</h1>
+                    <p>
+                        A build optimizer for Dark Souls II: Scholar of the
+                        First Sin
+                    </p>
+                </div>
+                <hr />
+
+                {/* Three columns, each about 30% of the page's width */}
+                <div className="app">
+                    {/* left column with armor info */}
+                    <article className="flex col-span-1 border rounded p-1 h-full">
+                        <h2 className="text-2xl font-bold">
+                            Armor info goes here
+                        </h2>
+                    </article>
+                    {/* main column with build info */}
+                    <article className="flex col-span-1 border rounded p-1 h-full">
+                        {/* <h2 className="text-3xl font-bold text-black dark:text-white">
+                            Character info goes here
+                        </h2> */}
+                        <BuildInfo />
+                    </article>
+                    {/* right column with weapon info */}
+                    <article className="flex col-span-1 border rounded p-1 h-full">
+                        <h2 className="text-2xl font-bold">
+                            Weapon info goes here
+                        </h2>
+                    </article>
+                </div>
+            </main>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+    );
 }
