4ee2fbabc3b020fa18ee3e53ce147477c7770ff9
Cameron Chrobocinski <cameronchrobo@gmail.com>
2026-07-22 21:59:35 -0500

    fixed poise calculation

--- lib/scripts/statCalculation.ts
+++ lib/scripts/statCalculation.ts
@@ -6,11 +6,44 @@ import {
 import AttributeMap, { AttributeMapKey } from "@/lib/types/attributeMap";
 import { StatMapKey } from "@/lib/types/statMap";
 
+function calculatePoise(adaptability: number, endurance: number): number {
+    let effectiveAttributeValue = adaptability;
+    let poise = 0;
+
+    // The effective attribute for poise is the lesser between adaptability and endurance
+    if (endurance < adaptability) {
+        effectiveAttributeValue = endurance;
+    }
+
+    //  Poise is increased by 0.3 per level until level 30, then 0.2 until level 50 and 0.1 per level thereafter
+    if (effectiveAttributeValue >= 0) {
+        poise += Math.min(30, effectiveAttributeValue) * 0.3;
+    }
+    if (effectiveAttributeValue > 30) {
+        poise += Math.min(20, effectiveAttributeValue - 30) * 0.2;
+    }
+    if (effectiveAttributeValue > 50) {
+        poise += (effectiveAttributeValue - 50) * 0.1;
+    }
+
+    // Round to two decimal places
+    poise = Math.round(poise * 100) / 100;
+    return poise;
+}
+
 export function calculateStat(
     statId: StatMapKey,
     attributes: AttributeMap<number>,
 ): number {
     let statValue = BaseStats[statId];
+
+    switch (statId) {
+        case "Poise":
+            return (
+                statValue +
+                calculatePoise(attributes.Adaptability, attributes.Endurance)
+            );
+    }
 
     for (const [key, value] of Object.entries(AttributeToStatMap)) {
         const isEffective = value[statId];
