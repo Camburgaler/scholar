5ece77ecc4175c7493425a71c0715d9e1e3c8346
Cameron Chrobocinski <cameronchrobo@gmail.com>
2026-07-27 16:26:21 -0500

    removed nullability from Equippable modifiers

--- lib/types/equippable.ts
+++ lib/types/equippable.ts
@@ -2,8 +2,8 @@ import ModifierMap from "@/lib/types/modifer";
 
 type Equippable = {
     Name: string; //pk
-    AdditiveModifiers?: ModifierMap | null;
-    MultiplicativeModifiers?: ModifierMap | null;
+    AdditiveModifiers?: ModifierMap;
+    MultiplicativeModifiers?: ModifierMap;
     Weight: number;
 };
 
