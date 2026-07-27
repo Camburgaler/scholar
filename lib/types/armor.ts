1163b03f50b143d09e7736993eebb37fc73664b2
Cameron Chrobocinski <cameronchrobo@gmail.com>
2026-07-27 16:27:08 -0500

    added the Requirements to the Armor type

--- lib/types/armor.ts
+++ lib/types/armor.ts
@@ -1,12 +1,15 @@
-import AbsorptionMap from "@/lib/types/absorptionMap";
+import ResistanceMap from "@/lib/types/absorptionMap";
 import DefenseMap from "@/lib/types/defenseMap";
 import Equippable from "@/lib/types/equippable";
+import AttributeMap from "./attributeMap";
 
 type Armor = Equippable & {
     Defenses: DefenseMap<number>;
-    Absorptions: AbsorptionMap<number>;
+    Resistances: ResistanceMap<number>;
     Poise: number;
-    Fitness?: number;
+    Requirements: AttributeMap<number>;
+
+    fitness?: number;
 };
 
 export default Armor;
