0f544d0c06e3a005ef0c24dc816fc12a7002faec
Cameron Chrobocinski <cameronchrobo@gmail.com>
2026-08-09 16:06:14 -0500

    added feature and bug templates added footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,59 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en" className={"h-full antialiased"}>
-            <body className="min-h-full flex flex-col">{children}</body>
+        <html lang="en" className="h-full antialiased box-border">
+            <body className="min-h-full flex flex-col">
+                {children}
+                <footer className="flex justify-between items-end mt-auto p-4">
+                    <span className="flex flex-col">
+                        <p>
+                            Inspired by{" "}
+                            <a
+                                href="https://www.mugenmonkey.com/darksouls2"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="underline"
+                            >
+                                MugenMonkey
+                            </a>
+                        </p>
+                    </span>
+                    <span className="flex flex-col text-right">
+                        <p>
+                            ©2026{" "}
+                            <a
+                                href="https://github.com/Camburgaler"
+                                className="underline"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Camburgaler
+                            </a>
+                        </p>
+                        <p>
+                            Have suggestions? Submit a{" "}
+                            <a
+                                href="https://github.com/Camburgaler/scholar/issues/new?template=feature_request.md"
+                                className="underline"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                feature request
+                            </a>{" "}
+                            or a{" "}
+                            <a
+                                href="https://github.com/Camburgaler/scholar/issues/new?template=bug_report.md"
+                                className="underline"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                bug report
+                            </a>
+                            !
+                        </p>
+                    </span>
+                </footer>
+            </body>
         </html>
     );
 }
