dbe266498eb0442eb265beba9230515ced9f8dbc
Cameron Chrobocinski <cameronchrobo@gmail.com>
2026-08-27 16:00:50 -0500

    replaced placeholder with actual weapon inventory

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import CharacterInfo from "@/lib/components/CharacterInfo";
+import WeaponInventory from "@/lib/components/WeaponInventory";
 import { EquippedArmorSetProvider } from "@/lib/reducers/equippedArmorSet";
 import { EquippedRingsProvider } from "@/lib/reducers/equippedRings";
 import { EquippedWeaponsProvider } from "@/lib/reducers/equippedWeapons";
@@ -73,41 +74,7 @@ export default function Home(): JSX.Element {
                                     </article>
                                     {/* right column with weapon info */}
                                     <article className="flex col-span-1 border rounded p-1 h-full">
-                                        <h2 className="text-2xl text-right font-bold">
-                                            Weapon Inventory
-                                        </h2>
-                                        {/* TODO: add weapon inventory container */}
-                                        {/* TODO: add weapon sorting algorithm */}
-                                        {/* TODO: add inventory system for weapons: */}
-                                        {/* TODO:     - every character has a weapon inventory */}
-                                        {/* TODO:     - inventory starts empty */}
-                                        {/* TODO:     - player can add instances of weapons to their character's inventory from a sortable list */}
-                                        {/* TODO:     - player can add the currently displayed starting class's weapons to their inventory with one button */}
-                                        {/* TODO:     - each weapon links out to a wiki page for it */}
-                                        {/* TODO:     - three views in weapon inventory: */}
-                                        {/* TODO:         - sorted weapons from within their inventory */}
-                                        {/* TODO:             - each weapon is "generic", meaning that it can have its infusion changed as necessary */}
-                                        {/* TODO:             - each weapon can be click-dragged onto a weapon slot to equip it */}
-                                        {/* TODO:             - each weapon can be interacted with to remove from inventory, infuse, or upgrade */}
-                                        {/* TODO:             - each weapon will show an icon next to it if it can be optimized via infusion */}
-                                        {/* TODO:                 - the icon will be that of the optimal infusion */}
-                                        {/* TODO:                 - to differentiate from the info that just shows what infusion the weapon currently has, there will be an exclamation point on the icon */}
-                                        {/* TODO:         - sorted list of all weapons */}
-                                        {/* TODO:             - each weapon is "specific", meaning that each weapon's infusion is listed as a separate weapon (see https://eldenring.tclark.io/) */}
-                                        {/* TODO:             - each weapon can be interacted with to add to inventory */}
-                                        {/* TODO:         - collapsible configs for sorting */}
-                                        {/* TODO:             - starts collapsed */}
-                                        {/* TODO:             - when opened, will expand into the empty space underneath the main three columns */}
-                                        {/* TODO:             - configure upgrade level (will only affect specific weapons) */}
-                                        {/* TODO:             - toggle for only showing weapons for which the character meets the requirements */}
-                                        {/* TODO:             - toggle for only showing buffable weapons */}
-                                        {/* TODO:             - toggle for showing weapons that use split damage */}
-                                        {/* TODO:             - toggle for considering status effects in sorting calculations */}
-                                        {/* TODO:             - filters based on infusion */}
-                                        {/* TODO:             - filters based on attack power types */}
-                                        {/* TODO:             - filters based on weapon category */}
-                                        {/* TODO:             - instructions/tips for sorting */}
-                                        <hr />
+                                        <WeaponInventory />
                                     </article>
                                 </div>
                             </main>
